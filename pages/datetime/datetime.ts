69fc58b82ec092fe66d64bcdb3a954b99e04ca12
Sivanraj <38311880+Sivan95@users.noreply.github.com>
2018-10-04 12:25:59 +0800

    Add files via upload

---
+++ pages/datetime/datetime.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl: 'datetime.html'
+})
+export class DatetimePage{
+    public event = {
+        month: '1990-02-19',
+        timeStarts: '07:43',
+        timeEnds: '1990-02-20'
+    }
+}
